b3dc7c07a0e6db925163c814a1661b37adbd6d7e
Benjamin Treglown <ben@Benjamins-MacBook-Air.local>
2023-11-20 17:32:00 +1100

    started editing layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,26 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+// application.js
+
+document.addEventListener('turbolinks:load', function () {
+  function displayImage(input) {
+    var preview = document.getElementById('preview-image');
+    var file = input.files[0];
+
+    if (file) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        preview.src = e.target.result;
+        preview.style.display = 'block';
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = '#';
+      preview.style.display = 'none';
+    }
+  }
+});
